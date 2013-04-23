b52aec28d8ad39a08d098e280286f5d9e48fc767
David Hoffmann <david@hoffmann-projects.de>
2013-04-23 23:13:28 +0200

    get random webbrowser
    Signed-off-by: David Hoffmann <david@hoffmann-projects.de>

--- src/GCGetData.py
+++ src/GCGetData.py
@@ -89,10 +89,15 @@ def RandomWait():
         logging.debug("RandomWait - Ende")
 
 def GetRandomUserAgent():
-    # TODO: Random
-    curBrowser = "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7"
-    # Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.91 Chrome/12.0.742.91 Safari/534.30
+    # list of browsers
+    browsers = [ "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.91 Chrome/12.0.742.91 Safari/534.30" ]
     
+    # randomize browsers
+    random.shuffle(browsers)
+
+    # get first random browser
+    curBrowser = browsers[0]
+
     if (DEBUG):
         logging.debug("Current Browser: " + curBrowser)
     
