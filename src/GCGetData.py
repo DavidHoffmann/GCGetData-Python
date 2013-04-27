37f650d0c51aced2d54b856c4e7fc9638514687a
David Hoffmann <david@hoffmann-projects.de>
2013-04-27 18:34:02 +0200

    add more user-agents
    Signed-off-by: David Hoffmann <david@hoffmann-projects.de>

--- src/GCGetData.py
+++ src/GCGetData.py
@@ -90,7 +90,7 @@ def RandomWait():
 
 def GetRandomUserAgent():
     # list of browsers
-    browsers = [ "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.91 Chrome/12.0.742.91 Safari/534.30" ]
+    browsers = [ "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.91 Chrome/12.0.742.91 Safari/534.30", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0" ]
     
     # randomize browsers
     random.shuffle(browsers)
